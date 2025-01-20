3794d48713e0c3d5201bb2ced80173c4cc10478c
alabzin <kard33@mail.ru>
2025-01-20 17:17:51 +0300

    edit build.gradle and models

--- src/test/java/models/UploadAppRsModel.java
+++ src/test/java/models/UploadAppRsModel.java
@@ -1,17 +1,11 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
-
+@Data
 public class UploadAppRsModel {
     @JsonProperty("app_url")
     private String appUrl;
 
-    public String getAppUrl() {
-        return appUrl;
-    }
-
-    public void setAppUrl(String appUrl) {
-        this.appUrl = appUrl;
-    }
 }
--- src/test/java/models/UploadedAppsListRsModel.java
+++ src/test/java/models/UploadedAppsListRsModel.java
@@ -1,8 +1,9 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
-
+@Data
 public class UploadedAppsListRsModel {
     @JsonProperty("app_name")
     private String appName;
@@ -14,44 +15,4 @@ public class UploadedAppsListRsModel {
     private String appId;
     @JsonProperty("uploaded_at")
     private String uploadedAt;
-
-    public String getAppName() {
-        return appName;
-    }
-
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
-
-    public String getAppVersion() {
-        return appVersion;
-    }
-
-    public void setAppVersion(String appVersion) {
-        this.appVersion = appVersion;
-    }
-
-    public String getAppUrl() {
-        return appUrl;
-    }
-
-    public void setAppUrl(String appUrl) {
-        this.appUrl = appUrl;
-    }
-
-    public String getAppId() {
-        return appId;
-    }
-
-    public void setAppId(String appId) {
-        this.appId = appId;
-    }
-
-    public String getUploadedAt() {
-        return uploadedAt;
-    }
-
-    public void setUploadedAt(String uploadedAt) {
-        this.uploadedAt = uploadedAt;
-    }
 }
