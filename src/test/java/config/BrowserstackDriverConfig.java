cdd74d099d35194160edce04ff3f5c0f6fcc7936
alabzin <kard33@mail.ru>
2025-01-23 13:46:50 +0300

    edit build.gradle and models

--- src/test/java/config/BrowserstackDriverConfig.java
+++ src/test/java/config/BrowserstackDriverConfig.java
@@ -7,14 +7,7 @@ import org.aeonbits.owner.Config;
 })
 public interface BrowserstackDriverConfig extends Config {
 
-    @Key("browserstack.user")
-    String getBrowserstackUser();
 
-    @Key("browserstack.key")
-    String getBrowserstackKey();
-
-    @Key("app")
-    String getApp();
 
     @Key("remoteUrl")
     String getRemoteUrl();
